ab8620925897836f4f1ca148668f4058486098a3
Anonuevo <geroanonuevo3@gmail.com>
2024-01-22 12:20:25 +0800

    trying to make for user inputs

--- prog.py
+++ prog.py
@@ -1,9 +1,11 @@
-#       01234567
-word = "pynative"
+import tkinter as tk
+from tkinter import simpledialog
 
-print("Orginal String is  pynative")
+word = str(tk.simpledialog.askstring("What is you word", "Word: "))
+
+print("Orginal String is ", word)
 print("Printing only even index chars")
 
-for arrangement in range(8):
+for arrangement in range():
     if arrangement%2 == 0:
         print(word[arrangement])
